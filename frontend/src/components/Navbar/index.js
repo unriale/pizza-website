453601cef2333e2f6ecf17e51a8ba9daad0dcc60
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2022-06-15 16:58:51 +0200

    created navbar

---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import { Nav, NavLink, NavIcon, Bars } from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>Pizza</NavLink>
+        <NavIcon>
+            <p>Menu</p>
+            <Bars/>
+        </NavIcon>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
