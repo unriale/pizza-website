f3d1f289b666ad00b786e3336dde6d55d734efe8
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2022-06-16 09:32:41 +0200

    added global styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
+import { GlobalStyles } from "./globalStyles";
 
 function App() {
   return (
     <Router>
+      <GlobalStyles/>
      <Navbar/>
     </Router>
   );
--- frontend/src/components/Navbar/NavbarElements.js
+++ frontend/src/components/Navbar/NavbarElements.js
@@ -26,20 +26,20 @@ export const NavLink = styled(Link)`
 `;
 
 export const NavIcon = styled.div`
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    cursor: pointer;
-    color: #fff;
+  display: block;
+  position: absolute;
+  top: 0;
+  right: 0;
+  cursor: pointer;
+  color: #fff;
 
-    p {
-        transform: translate(-175%, 100%);
-        font-weight: bold;
-    }
-`
+  p {
+    transform: translate(-175%, 100%);
+    font-weight: bold;
+  }
+`;
 
 export const Bars = styled(FaPizzaSlice)`
-    font-size: 2rem;
-    transform: translate(-50%, -15%);
-`
+  font-size: 2rem;
+  transform: translate(-50%, -15%);
+`;
