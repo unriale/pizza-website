088de9d8f57500a6882428783db884be4fe74f36
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2022-06-16 14:02:52 +0200

    added menu sidebar

--- frontend/src/components/Hero/index.js
+++ frontend/src/components/Hero/index.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Navbar from "../Navbar";
+import Sidebar from "../Sidebar";
 import {
   HeroContainer,
   HeroContent,
@@ -9,9 +11,15 @@ import {
 } from "./HeroElements";
 
 const Hero = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <HeroContainer>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
       <HeroContent>
         <HeroItems>
           <HeroH1>Greatest Pizza Ever</HeroH1>
--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,11 +1,11 @@
 import { Nav, NavLink, NavIcon, Bars } from "./NavbarElements";
 
-const Navbar = () => {
+const Navbar = ({toggle}) => {
   return (
     <>
       <Nav>
         <NavLink to='/'>Pizza</NavLink>
-        <NavIcon>
+        <NavIcon onClick={toggle}>
             <p>Menu</p>
             <Bars/>
         </NavIcon>
