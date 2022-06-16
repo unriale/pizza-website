a19241da3b2b97632481d9d1d688407b7f516408
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2022-06-16 16:11:56 +0200

    new products section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import Hero from "./components/Hero";
 import Products from "./components/Products";
+import Feature from "./components/Feature";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyles } from "./globalStyles";
-import { productData } from "./components/Products/data";
+import { productData, productData2 } from "./components/Products/data";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <GlobalStyles />
       <Hero />
       <Products heading="Choose your favorite" data={productData} />
+      <Feature />
+      <Products heading="Sweet Treats for You" data={productData2} />
     </Router>
   );
 }
--- frontend/src/components/Products/data.js
+++ frontend/src/components/Products/data.js
@@ -2,6 +2,10 @@ import product1 from "../../images/product-1.jpg";
 import product2 from "../../images/product-2.jpg";
 import product3 from "../../images/product-3.jpg";
 
+import sweet1 from "../../images/sweet1.jpg";
+import sweet2 from "../../images/sweet2.jpg";
+import sweet3 from "../../images/sweet3.jpg";
+
 export const productData = [
   {
     img: product1,
@@ -25,6 +29,33 @@ export const productData = [
     name: "Veggie Overload",
     desc: "Marinara sauce, basil, italian sausage, tomatoes, olives",
     price: "$14.99",
+    button: "Add to Cart",
+  },
+];
+
+export const productData2 = [
+  {
+    img: sweet2,
+    alt: "Donuts",
+    name: "Doughlicious",
+    desc: "Belgian chocolate glazed donuts covered in icing with sprinkles on top",
+    price: "$9.99",
+    button: "Add to Cart",
+  },
+  {
+    img: sweet3,
+    alt: "Ice Cream",
+    name: "Caramel Wonder",
+    desc: "Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick",
+    price: "$12.99",
+    button: "Add to Cart",
+  },
+  {
+    img: sweet1,
+    alt: "Brownie",
+    name: "Brownie Bunch",
+    desc: "Double fudge brownie squares topped with white chocolate pieces and macadamia nuts",
+    price: "$9.99",
     button: "Add to Cart",
   },
 ];
